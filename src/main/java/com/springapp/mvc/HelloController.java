0b14d7f33000c395181f2c7292b08db0906d3780
michael-hgs <whitewolf042495@gmail.com>
2018-01-28 15:28:46 +0200

    push

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -16,20 +16,20 @@ public class HelloController {
 
 //		System.out.println(“Sending an iOS push notificatio”);
 
-		ApnsService service = APNS.newService()
-				.withCert("/Users/michael/IdeaProjects/CheckPush/src/check.p12", "1234")
-				.withSandboxDestination()
-				.build();
-
-		String payload = APNS.newPayload()
-				.alertBody("Cant be simpler than this!").sound("default")
-				.alertTitle("test alert title").build();
+//		ApnsService service = APNS.newService()
+//				.withCert("/Users/michael/IdeaProjects/CheckPush/src/check.p12", "1234")
+//				.withSandboxDestination()
+//				.build();
 //
-		String token = "f36172e3301f6c5fa35011528f4527b1940af25d7adb9d94286ca87a88dcf6e0";
-//
-//		System.out.println(“payload: “+payload);
-//
-		service.push(token, payload);
+//		String payload = APNS.newPayload()
+//				.alertBody("Cant be simpler than this!").sound("default")
+//				.alertTitle("test alert title").build();
+////
+//		String token = "f36172e3301f6c5fa35011528f4527b1940af25d7adb9d94286ca87a88dcf6e0";
+////
+////		System.out.println(“payload: “+payload);
+////
+//		service.push(token, payload);
 //
 //		System.out.println(“The message has been hopefully sent…”);
 
