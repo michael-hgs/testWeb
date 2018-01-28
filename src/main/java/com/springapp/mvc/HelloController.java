69aec5480338925e4073145895cb1cd4f53afe56
michael-hgs <whitewolf042495@gmail.com>
2018-01-28 19:03:24 +0200

    last check

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -19,7 +19,7 @@ import java.security.CodeSource;
 public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
+		model.addAttribute("message", "sfdghgkjlk world!");
 
 //		System.out.println(“Sending an iOS push notificatio”);
 //		CodeSource codeSource = HelloController.class.getProtectionDomain().getCodeSource();
